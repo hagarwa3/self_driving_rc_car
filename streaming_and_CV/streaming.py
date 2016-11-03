04524a1729de4eea24dc73714a0715798ef20415
Evan Fabry <Evan.Fabry@gmail.com>
2016-11-03 13:22:17 -0500

    Updated comments and style

--- streaming_and_CV/streaming.py
+++ streaming_and_CV/streaming.py
@@ -3,22 +3,26 @@ import urllib.request
 import numpy as np
 
 
-evanURL = 'http://192.168.1.22/live'
-URL = 'http://192.168.1.5:8080/video'
-urlIllinoisNet = 'http://10.194.9.154:8080/video'
+evan_url = 'http://192.168.1.22/live'                   # URL for Evan's video stream
+url = 'http://192.168.1.5:8080/video'                   # video for Harhit's stream
+url_illinois_net = 'http://10.194.9.154:8080/video'     # video for Harshit's Siebel stream
+evan_url_illinois_net = 'http://10.192.224.222/'        # video for Evan's Siebel stream
 
 show_grayscale = True
 
-stream = urllib.request.urlopen(urlIllinoisNet)
+# Open the URL
+stream = urllib.request.urlopen(evan_url_illinois_net)
 bytes = bytes()
 while True:
     bytes += stream.read(1024)
     start_idx = bytes.find(b'\xff\xd8')
     end_idx = bytes.find(b'\xff\xd9')
     if start_idx != -1 and end_idx != -1:
+        # If valid frame
         jpg = bytes[start_idx : end_idx + 2]
         bytes = bytes[end_idx + 2 :]
         img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
+
         if not show_grayscale:
             cv2.imshow('i', img)
         else:
