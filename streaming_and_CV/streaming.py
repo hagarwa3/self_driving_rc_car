60dcba3ffb4fda5e934499497c0af60e42b82b89
Harshit Agarwal <harshit@wirelessprv-10-192-24-141.near.illinois.edu>
2016-11-03 12:08:39 -0500

    fixed streaming

--- streaming_and_CV/streaming.py
+++ streaming_and_CV/streaming.py
@@ -5,10 +5,11 @@ import numpy as np
 
 evanURL = 'http://192.168.1.22/live'
 URL = 'http://192.168.1.5:8080/video'
+urlIllinoisNet = 'http://10.194.9.154:8080/video'
 
 show_grayscale = True
 
-stream = urllib.request.urlopen(evanURL)
+stream = urllib.request.urlopen(urlIllinoisNet)
 bytes = bytes()
 while True:
     bytes += stream.read(1024)
@@ -17,12 +18,11 @@ while True:
     if start_idx != -1 and end_idx != -1:
         jpg = bytes[start_idx : end_idx + 2]
         bytes = bytes[end_idx + 2 :]
-
+        img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.IMREAD_COLOR)
         if not show_grayscale:
-            img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8))
             cv2.imshow('i', img)
         else:
-            gray_image = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+            gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             cv2.imshow('i', gray_image)
 
         if cv2.waitKey(1) == 27:
