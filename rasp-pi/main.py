633026f777d7d4a375cc87223f543d3d283e8696
Harshit Agarwal <harshit@Harshits-MacBook-Pro.local>
2016-11-03 04:14:00 -0500

    updated raspberry pi flask

--- rasp-pi/main.py
+++ rasp-pi/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from flask.ext.cors import CORS
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={"/*": {"origins": "*"}})
@@ -7,24 +7,34 @@ cors = CORS(app, resources={"/*": {"origins": "*"}})
 
 @app.route('/speed/', defaults={'path': ''}, methods=['POST'])
 def set_speed():
-	"""
-	This is an endpoint to set the speed of the raspberry pi from 
-	an HTTP post request.
-	"""
+    """
+    This is an endpoint to set the speed of the raspberry pi from 
+    an HTTP post request.
+    """
     speed = request.args.get('speed')
     print(speed)
     return speed
 
 
 @app.route('/turn/', defaults={'path': ''}, methods=['GET'])
-def set_speed():
-	"""
-	This is an endpoint to cause the raspberry pi to turn by some
-	real number of degrees.
-	"""
-    angel = request.args.get('angle')
-    print(angle)
-    return angle
+def set_direction():
+    """
+    This is an endpoint to cause the raspberry pi to turn in some direction. 
+    Exact implementation to be figured out with an actual arduino.
+    """
+    direction = request.args.get('direction')
+    """
+    if direction == "left":
+        arduino execute left turn
+    elif direction == "right":
+        arduino execute right turn
+    elif direction == "forward":
+        arduino move forward
+    elif direction == "reverse":
+        arduino move backwards
+    """
+    print(direction)
+    return direction
 
 
 if __name__ == "__main__":
