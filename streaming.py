01385e132aee71e4a53b388b527f71689c9c1782
Harshit Agarwal <harshit@Harshits-MacBook-Pro.local>
2016-11-03 03:16:09 -0500

    initial streaming

---
+++ streaming.py
@@ -0,0 +1,20 @@
+import cv2
+import urllib.request
+import numpy as np
+
+
+evanURL = 'http://192.168.1.22/live'
+URL = 'http://192.168.1.5:8080/video'
+stream = urllib.request.urlopen(evanURL)
+bytes = bytes()
+while True:
+    bytes += stream.read(1024)
+    a = bytes.find(b'\xff\xd8')
+    b = bytes.find(b'\xff\xd9')
+    if a != -1 and b != -1:
+        jpg = bytes[a:b+2]
+        bytes = bytes[b+2:]
+        i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_GRAYSCALE)
+        cv2.imshow('i', i)
+        if cv2.waitKey(1) == 27:
+            exit(0)
