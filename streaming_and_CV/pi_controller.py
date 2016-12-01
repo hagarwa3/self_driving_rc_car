826b4ac6f111c12739f38e6dce7808037f799e21
Evan Fabry <Evan.Fabry@gmail.com>
2016-12-01 02:45:57 -0600

    Fixed some things

--- streaming_and_CV/pi_controller.py
+++ streaming_and_CV/pi_controller.py
@@ -2,50 +2,59 @@ import requests
 
 
 class PiController:
-    def __init__(address="http://192.168.1.11:9876/"):
+    def __init__(self, address="http://192.168.1.11:9876/"):
         self.address = address
         self.last_direction = None
         self.direction = None
 
-    def set_direction(new_directon):
+    def set_direction(self, new_directon):
         self.direction = new_directon
         self.last_direction = self.direction
 
-    def check_speed():
+    def check_speed(self):
         if self.last_direction == "stop" and self.direction != "stop":
-            requests.post(bot_url + "/speed", data={"speed": "9"})
+            requests.post(self.address + "/speed", data={"speed": "9"})
 
-    def stop():
-        requests.post(bot_url + "/speed", data={"speed": "0"})
-        requests.post(bot_url + "/turn", data={"direction": "forward"})
+    def stop(self):
+        requests.post(self.address + "/speed", data={"speed": "0"})
+        requests.post(self.address + "/turn", data={"direction": "forward"})
         self.set_direction("stop")
 
-    def right():
+    def right(self):
         self.check_speed()
-        requests.post(bot_url + "/turn", data={"direction": "right"})
+        requests.post(self.address + "/turn", data={"direction": "right"})
         self.set_direction("right")
 
-    def left():
+    def left(self):
         self.check_speed()
-        requests.post(bot_url + "/turn", data={"direction": "left"})
+        requests.post(self.address + "/turn", data={"direction": "left"})
         self.set_direction("left")
 
-    def forwards():
+    def forwards(self):
         self.check_speed()
-        requests.post(bot_url + "/turn", data={"direction": "forward"})
+        requests.post(self.address + "/turn", data={"direction": "forward"})
         self.set_direction("forward")
 
-    def forward_right():
+    def forward_right(self):
         self.check_speed()
-        requests.post(bot_url + "/turn", data={"direction": "forward right"})
+        requests.post(self.address + "/turn", data={"direction": "forward right"})
         self.set_direction("forward right")
 
-    def forward_left():
+    def forward_left(self):
         self.check_speed()
-        requests.post(bot_url + "/turn", data={"direction": "forward left"})
+        requests.post(self.address + "/turn", data={"direction": "forward left"})
         self.set_direction("forward left")
 
-    def reverse():
+    def reverse(self):
         self.check_speed()
-        requests.post(bot_url + "/turn", data={"direction": "reverse"})
+        requests.post(self.address + "/turn", data={"direction": "reverse"})
         self.set_direction("reverse")
+
+    def __del__(self):
+        self.stop()
+
+
+if __name__ == "__main__":
+    conn = PiController()
+    print(conn.address)
+    conn.forwards()
